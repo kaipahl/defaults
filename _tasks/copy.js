74386ea846061ecab202bafbd05f654817763cf8
Kai Pahl <kai@kaipahl.de>
2019-12-03 22:13:23 +0100

    Add copy task into the new task runner structure

---
+++ _tasks/copy.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+
+let cpy = require('cpy');
+
+/**
+ * For some file types it copies the comple folder structure
+ * It uses the Glob syntax, known from Grunt.
+ *
+ * @example ./src/_fonts/foo.woff --> ./dist/_fonts/foo.woff
+ */
+(async () => {
+	await cpy(
+		[
+			'./**/*.{php,html}',
+			'./**/*.{ttf,woff}',
+			'./**/*.{gif,png,jpg,jpeg,pdf,ico}'
+		],
+		'../dist',
+		{
+			cwd: './src', // set scope
+			parents: true // conserve directory structure
+		}
+	);
+	console.log('Files copied!');
+})();
